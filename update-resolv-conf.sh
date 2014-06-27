60928367d507ee2c9825b8c3505a3f87d15ea333
Alfredo Palhares <masterkorp@masterkorp.net>
2014-06-27 16:51:39 +0100

    Fix identation issues.
    Tab size is now 2.
    Fix mismatch between 3 and 2 spaces.

--- update-resolv-conf.sh
+++ update-resolv-conf.sh
@@ -31,34 +31,34 @@ fi
 case $script_type in
 
 up)
-   for optionname in ${!foreign_option_*} ; do
-      option="${!optionname}"
-      echo $option
-      part1=$(echo "$option" | cut -d " " -f 1)
-      if [ "$part1" == "dhcp-option" ] ; then
-         part2=$(echo "$option" | cut -d " " -f 2)
-         part3=$(echo "$option" | cut -d " " -f 3)
-         if [ "$part2" == "DNS" ] ; then
-            IF_DNS_NAMESERVERS="$IF_DNS_NAMESERVERS $part3"
-         fi
-         if [[ "$part2" == "DOMAIN" || "$part2" == "DOMAIN-SEARCH" ]] ; then
-            IF_DNS_SEARCH="$IF_DNS_SEARCH $part3"
-         fi
+  for optionname in ${!foreign_option_*} ; do
+    option="${!optionname}"
+    echo $option
+    part1=$(echo "$option" | cut -d " " -f 1)
+    if [ "$part1" == "dhcp-option" ] ; then
+      part2=$(echo "$option" | cut -d " " -f 2)
+      part3=$(echo "$option" | cut -d " " -f 3)
+      if [ "$part2" == "DNS" ] ; then
+        IF_DNS_NAMESERVERS="$IF_DNS_NAMESERVERS $part3"
       fi
-   done
-   R=""
-   for DS in $IF_DNS_SEARCH ; do
-           R="${R}search $DS
+      if [[ "$part2" == "DOMAIN" || "$part2" == "DOMAIN-SEARCH" ]] ; then
+        IF_DNS_SEARCH="$IF_DNS_SEARCH $part3"
+      fi
+    fi
+  done
+  R=""
+  for DS in $IF_DNS_SEARCH ; do
+    R="${R}search $DS
 "
-   done
-   for NS in $IF_DNS_NAMESERVERS ; do
-           R="${R}nameserver $NS
+  done
+  for NS in $IF_DNS_NAMESERVERS ; do
+    R="${R}nameserver $NS
 "
-   done
-   #echo -n "$R" | $RESOLVCONF -p -a "${dev}"
-   echo -n "$R" | $RESOLVCONF -a "${dev}.inet"
-   ;;
+  done
+  #echo -n "$R" | $RESOLVCONF -p -a "${dev}"
+  echo -n "$R" | $RESOLVCONF -a "${dev}.inet"
+  ;;
 down)
-   $RESOLVCONF -d "${dev}.inet"
-   ;;
+  $RESOLVCONF -d "${dev}.inet"
+  ;;
 esac
